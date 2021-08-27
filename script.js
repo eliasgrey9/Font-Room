1dd0ee08c50d4743c3a7183ba67beedcd5b7a4d8
eliasgrey9 <89663439+eliasgrey9@users.noreply.github.com>
2021-08-27 13:13:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function annotate(){
+  var typed= document.getElementById("main").value;
+  document.getElementById("lobster").value= typed;
+  document.getElementById("grandHotel").value= typed;
+  document.getElementById("yesteryear").value= typed;
+  document.getElementById("cookie").value= typed;
+   document.getElementById("mrsSheppards").value= typed;
+  document.getElementById("pacifico").value= typed;
+  document.getElementById("yellowTail").value= typed;
+  document.getElementById("norican").value= typed;
+  document.getElementById("molle").value= typed;
+  
+  
+  
+  
+  
+}
